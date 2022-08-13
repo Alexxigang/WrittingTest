dcc14714ceb596c62ba69edf208c23274c5b7a6e
Alexxigang <2585192282@qq.com>
2022-08-13 09:51:01 +0800

    feat: feat microSoft test

---
+++ src/main/java/com/write/code/realgang/microsoftTest/testOne/Main.java
@@ -0,0 +1,12 @@
+package com.write.code.realgang.microsoftTest.testOne;
+
+/**
+ * @description: main
+ * @author: RealGang
+ * @create: 2022-08-13 09:49
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
