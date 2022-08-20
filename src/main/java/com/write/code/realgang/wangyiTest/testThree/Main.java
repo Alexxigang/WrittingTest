2dd65e062cbf2c20a9694ec0daa349c6957deece
Alexxigang <2585192282@qq.com>
2022-08-20 15:53:32 +0800

    aaa

---
+++ src/main/java/com/write/code/realgang/wangyiTest/testThree/Main.java
@@ -0,0 +1,16 @@
+package com.write.code.realgang.wangyiTest.testThree;
+
+/**
+ * @description: main
+ * @author: RealGang
+ * @create: 2022-08-13 09:49
+ **/
+public class Main {
+    public void solution() {
+    }
+
+    public static void main(String[] args) {
+        Main mainClass = new Main();
+        mainClass.solution();
+    }
+}
