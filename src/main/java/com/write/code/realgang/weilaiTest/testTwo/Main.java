1569e2e11eb4e8606b7f555f2d32db3390214c4f
Alexxigang <2585192282@qq.com>
2022-08-30 21:19:46 +0800

    feat

--- src/main/java/com/write/code/realgang/weilaiTest/testTwo/Main.java
+++ src/main/java/com/write/code/realgang/weilaiTest/testTwo/Main.java
@@ -1,9 +1,31 @@
 package com.write.code.realgang.weilaiTest.testTwo;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @description: main
  * @author: RealGang
  * @create: 2022-08-30 20:06
  **/
 public class Main {
+
+    public void solution() {
+        Scanner reader = new Scanner(System.in);
+        int n = reader.nextInt();
+        List<int[]> list = new LinkedList<>();
+        for (int i = 0;i < n;i++) {
+            int[] arr = new int[2];
+            arr[0] = reader.nextInt();
+            arr[1] = reader.nextInt();
+            list.add(arr);
+        }
+
+        System.out.println(9);
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.solution();
+    }
 }
