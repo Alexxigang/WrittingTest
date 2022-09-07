25eaebd90742fcbd23ac80f2e0b3e436d1595405
Alexxigang <2585192282@qq.com>
2022-09-07 20:37:03 +0800

    fff

--- src/main/java/com/write/code/realgang/huaweiTestTwo/testTwo/Main.java
+++ src/main/java/com/write/code/realgang/huaweiTestTwo/testTwo/Main.java
@@ -1,12 +1,34 @@
 package com.write.code.realgang.huaweiTestTwo.testTwo;
 
+import java.util.Scanner;
+
 /**
  * @description: main
  * @author: RealGang
  * @create: 2022-09-07 19:14
  **/
 public class Main {
-    public static void main(String[] args) {
 
+    public void solution() {
+        Scanner reader = new Scanner(System.in);
+        int M = reader.nextInt();
+        int N = reader.nextInt();
+
+        char[][] block = new char[M][N];
+        reader.nextLine();
+        for (int i = 0;i < M;i++) {
+            String s = reader.nextLine();
+            char[] chars = s.toCharArray();
+            for (int j = 0;j < N;j++) {
+                block[i][j] = chars[j];
+            }
+        }
+
+        System.out.println(111);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.solution();
     }
 }
