a7cdd7fb3fdff7df66b196867e387f8a6f9a0db4
Alexxigang <2585192282@qq.com>
2022-09-07 19:23:42 +0800

    fff

---
+++ src/main/java/com/write/code/realgang/huaweiTestTwo/testOne/Main.java
@@ -0,0 +1,22 @@
+package com.write.code.realgang.huaweiTestTwo.testOne;
+
+import java.util.Scanner;
+
+/**
+ * @description: main
+ * @author: RealGang
+ * @create: 2022-09-07 19:14
+ **/
+public class Main {
+    public void solution() {
+        Scanner reader = new Scanner(System.in);
+        int N = reader.nextInt();
+        int M = reader.nextInt();
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/write/code/realgang/huaweiTestTwo/testTwo/Main.java
@@ -0,0 +1,12 @@
+package com.write.code.realgang.huaweiTestTwo.testTwo;
+
+/**
+ * @description: main
+ * @author: RealGang
+ * @create: 2022-09-07 19:14
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
