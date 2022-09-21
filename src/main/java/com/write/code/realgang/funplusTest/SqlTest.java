97ae4d4fcfd97b4a179b9138cc5919054d045963
Alexxigang <2585192282@qq.com>
2022-09-21 20:23:27 +0800

    fff

---
+++ src/main/java/com/write/code/realgang/funplusTest/SqlTest.java
@@ -0,0 +1,11 @@
+package com.write.code.realgang.funplusTest;
+
+/**
+ * @description:
+ * @author: RealGang
+ * @create: 2022-09-21 19:32
+ **/
+public class SqlTest {
+
+
+}
---
+++ src/main/java/com/write/code/realgang/lazadaTest/TestOne.java
@@ -0,0 +1,41 @@
+package com.write.code.realgang.lazadaTest;
+
+/**
+ * @description:
+ * @author: RealGang
+ * @create: 2022-09-21 20:22
+ **/
+public class TestOne {
+    public int nextGreaterElement(int n) {
+        char[] nums = Integer.toString(n).toCharArray();
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i < 0) {
+            return -1;
+        }
+        int j = nums.length - 1;
+        while (i < j && nums[i] >= nums[j]) {
+            j--;
+        }
+        swap(nums,i,j);
+        reverse(nums,i + 1);
+        long res = Long.parseLong(new String(nums));
+        return res > Integer.MAX_VALUE ? -1 : (int)res;
+    }
+
+    public void swap(char[] nums,int i,int j) {
+        char temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void reverse(char[] nums,int begin) {
+        int i = begin,j = nums.length - 1;
+        while (i < j) {
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+}
