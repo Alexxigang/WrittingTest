7acc44b6783e605110d4f7260fa3f9cb24ee7159
Alexxigang <2585192282@qq.com>
2022-09-25 18:26:23 +0800

    fff

--- src/main/java/com/write/code/realgang/boleTest/ShellSort.java
+++ src/main/java/com/write/code/realgang/boleTest/ShellSort.java
@@ -6,38 +6,38 @@ package com.write.code.realgang.boleTest;
  * @create: 2022-09-25 16:30
  **/
 public class ShellSort {
-    # shell_sort 代码实现
-
-from typing import List
-
-def shell_sort(arr:List[int]):
-    """
-    希尔排序
-    :param arr: 待排序的List
-    :return: 希尔排序是就地排序(in-place)
-    """
-    length = len(arr)
-    dist = length // 2
-
-    while dist > 0:
-        for i in range(dist, length):
-            temp = arr[i]
-            j = i
-            while j >= dist and temp < arr[j-dist]:
-                arr[j] = arr[j-dist]
-                j -= dist
-            arr[j] = temp
-        dist //= 2
-
-# 测试数据
-
-if __name__ == '__main__':
-    import random
-    random.seed(54)
-    arr = [random.randint(0,100) for _ in range(10)]
-    print("原始数据：", arr)
-    shell_sort(arr)
-    print("希尔排序结果：", arr)
+//    # shell_sort 代码实现
+//
+//from typing import List
+//
+//def shell_sort(arr:List[int]):
+//    """
+//    希尔排序
+//    :param arr: 待排序的List
+//    :return: 希尔排序是就地排序(in-place)
+//    """
+//    length = len(arr)
+//    dist = length // 2
+//
+//    while dist > 0:
+//        for i in range(dist, length):
+//            temp = arr[i]
+//            j = i
+//            while j >= dist and temp < arr[j-dist]:
+//                arr[j] = arr[j-dist]
+//                j -= dist
+//            arr[j] = temp
+//        dist //= 2
+//
+//# 测试数据
+//
+//if __name__ == '__main__':
+//    import random
+//    random.seed(54)
+//    arr = [random.randint(0,100) for _ in range(10)]
+//    print("原始数据：", arr)
+//    shell_sort(arr)
+//    print("希尔排序结果：", arr)
 
 }
 
--- src/main/java/com/write/code/realgang/boleTest/TestTwo.java
+++ src/main/java/com/write/code/realgang/boleTest/TestTwo.java
@@ -7,50 +7,50 @@ package com.write.code.realgang.boleTest;
  **/
 public class TestTwo {
 
-    from collections import defaultdict
-
-
-class Trie:
-    def __init__(self):
-        self.children = defaultdict(Trie)
-        self.word = ""
-
-    def insert(self, word):
-        cur = self
-        for c in word:
-            cur = cur.children[c]
-        cur.is_word = True
-        cur.word = word
-
-
-class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        trie = Trie()
-        for word in words:
-            trie.insert(word)
-
-        def dfs(now, i1, j1):
-            if board[i1][j1] not in now.children:
-                return
-
-            ch = board[i1][j1]
-
-            now = now.children[ch]
-            if now.word != "":
-                ans.add(now.word)
-
-            board[i1][j1] = "#"
-            for i2, j2 in [(i1 + 1, j1), (i1 - 1, j1), (i1, j1 + 1), (i1, j1 - 1)]:
-                if 0 <= i2 < m and 0 <= j2 < n:
-                    dfs(now, i2, j2)
-            board[i1][j1] = ch
-
-        ans = set()
-        m, n = len(board), len(board[0])
-
-        for i in range(m):
-            for j in range(n):
-                dfs(trie, i, j)
-
-        return list(ans)
+//    from collections import defaultdict
+//
+//
+//class Trie:
+//    def __init__(self):
+//        self.children = defaultdict(Trie)
+//        self.word = ""
+//
+//    def insert(self, word):
+//        cur = self
+//        for c in word:
+//            cur = cur.children[c]
+//        cur.is_word = True
+//        cur.word = word
+//
+//
+//class Solution:
+//    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+//        trie = Trie()
+//        for word in words:
+//            trie.insert(word)
+//
+//        def dfs(now, i1, j1):
+//            if board[i1][j1] not in now.children:
+//                return
+//
+//            ch = board[i1][j1]
+//
+//            now = now.children[ch]
+//            if now.word != "":
+//                ans.add(now.word)
+//
+//            board[i1][j1] = "#"
+//            for i2, j2 in [(i1 + 1, j1), (i1 - 1, j1), (i1, j1 + 1), (i1, j1 - 1)]:
+//                if 0 <= i2 < m and 0 <= j2 < n:
+//                    dfs(now, i2, j2)
+//            board[i1][j1] = ch
+//
+//        ans = set()
+//        m, n = len(board), len(board[0])
+//
+//        for i in range(m):
+//            for j in range(n):
+//                dfs(trie, i, j)
+//
+//        return list(ans)
 }
