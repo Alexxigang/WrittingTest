06e55fd26a79e27298ec265070a273f4c38c9c1c
Andrew Lau <13995708359@163.com>
2022-09-27 19:59:54 +0800

    ..

---
+++ src/main/java/com/write/code/ruiqiang/antTest/testTwo/Main.java
@@ -0,0 +1,36 @@
+package com.write.code.ruiqiang.antTest.testTwo;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public void solute(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] nums = new int[n];
+        Arrays.sort(nums);
+        Set st = new HashSet();
+        for(int i=0;i<n;i++){
+            st.add(nums[i]);
+        }
+        int count = 0;
+        for(int i=1;i<n;i++){
+            if(nums[i] == nums[i-1]){
+                while(st.contains(nums[i])){
+                    nums[i] += k;
+                    count += 1;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Main ma = new Main();
+        ma.solute();
+    }
+}
