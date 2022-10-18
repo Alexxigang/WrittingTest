a0965c697176d06082b1601f213a4c93c1e66a11
Alexxigang <2585192282@qq.com>
2022-10-18 19:46:33 +0800

    aaa

---
+++ src/main/java/com/write/code/realgang/huaweiTestTwo/NumSum.java
@@ -0,0 +1,9 @@
+package com.write.code.realgang.huaweiTestTwo;/**
+* @description: 
+*
+* @author: RealGang
+*
+* @create: 2022-10-12 11:24
+**/
+public class NumSum {
+}
---
+++ src/main/java/com/write/code/realgang/xiechengTest/testTwo/Main.java
@@ -0,0 +1,9 @@
+package com.write.code.realgang.xiechengTest.testTwo;/**
+* @description: main
+*
+* @author: RealGang
+*
+* @create: 2022-10-18 19:27
+**/
+public class Main {
+}
